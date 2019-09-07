47683ac522b6558a66c9cb99d965bd2f38ef3a7e
Hanna Shevelova <anna.v8848@gmail.com>
2019-09-07 16:33:26 +0300

    formatted

--- defines.h
+++ defines.h
@@ -6,46 +6,46 @@
 /*   By: gvynogra <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/07 13:16:53 by gvynogra          #+#    #+#             */
-/*   Updated: 2019/09/07 13:16:55 by gvynogra         ###   ########.fr       */
+/*   Updated: 2019/09/07 16:32:50 by gvynogra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef AVM_DEFINES_H
-#define AVM_DEFINES_H
+# define AVM_DEFINES_H
 
 typedef enum {
-    INSTRUCTION,
-    OPERAND,
-    ERROR,
-    END
-} eTokenType;
+	INSTRUCTION,
+	OPERAND,
+	ERROR,
+	END
+}	eTokenType;
 
 typedef enum
 {
-    Int8,
-    Int16,
-    Int32,
-    Float,
-    Double,
-    Types
-} eOperandType;
+	Int8,
+	Int16,
+	Int32,
+	Float,
+	Double,
+	Types
+}	eOperandType;
 
 typedef enum
 {
-    PUSH,
-    POP,
-    DUMP,
-    ASSERT,
-    ADD,
-    SUB,
-    MUL,
-    DIV,
-    MOD,
-    PRINT,
-    EXIT,
-    CLOSEINPUT,
-    COMMENT,
-    WORDS
-} eKeyword;
+	PUSH,
+	POP,
+	DUMP,
+	ASSERT,
+	ADD,
+	SUB,
+	MUL,
+	DIV,
+	MOD,
+	PRINT,
+	EXIT,
+	CLOSEINPUT,
+	COMMENT,
+	WORDS
+}	eKeyword;
 
 #endif //AVM_DEFINES_H
